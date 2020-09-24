515cbbda6e1a3dba94e35a9857d03b267ceeab86
Jonel Ccente Zuzunaga <jonel.cz@gmail.com>
2020-09-24 10:13:04 -0500

    programming assigmment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix build a special vector with four functions and cached 
+## inverse matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  sol <- NULL
+  set <- function(y) {
+    x <<- y
+    sol <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(msolve) sol <<- msolve
+  getsolve <- function() sol
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## cache solve resolve inverse matrix using a vector builded with 
+## makeCacheMatrix. if inverse is calculated in previous step return 
+## the value of cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  sol <- x$getsolve()
+  if(!is.null(sol)) {
+    message("getting cached data")
+    return(sol)
+  }
+  data <- x$get()
+  sol <- solve(data, ...)
+  x$setsolve(sol)
+  sol
 }
